24bb727598f9fb5cb6c5e0ce4f390ca9b4836bc6
123bangjack01 <128490612+123bangjack01@users.noreply.github.com>
2026-05-17 20:29:07 +0700

    add railway config

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Install dependency"
+composer install --no-dev --optimize-autoloader --ignore-platform-req=ext-gd
+
+echo "Generate key"
+php artisan key:generate
+
+echo "Cache config"
+php artisan config:cache
+
+echo "Run migration"
+php artisan migrate --force
+
+echo "Start Laravel"
+php artisan serve --host=0.0.0.0 --port=$PORT
